9aae29d4bc4328bc50d2795330900a94e533de67
tmm6 <tmm6@cesar.school>
2023-02-25 16:22:15 -0300

    Refatoração.

--- src/models/ice_cream_stand.py
+++ src/models/ice_cream_stand.py
@@ -15,41 +15,64 @@ class IceCreamStand(Restaurant):
     # trocar o print pelo return
     def flavors_available(self):
         """Percorra a lista de sabores disponíveis e imprima."""
+        """
+        Correção:
+        - Trocar print por return.
+        - Ajustar a mensagem para retornar a frase e os valores.
+        
+        Remoção:
+        print("\nNo momento temos os seguintes sabores de sorvete disponíveis:")
+        print("Estamos sem estoque atualmente!")
+        for flavor in self.flavors:
+            print(f"\t-{flavor}")
+        """
         if self.flavors:
-            # refatorando para ter um return
-            #print("\nNo momento temos os seguintes sabores de sorvete disponíveis:")
-            return self.flavors
-            # for flavor in self.flavors:
-            #     print(f"\t-{flavor}")
+            return f'No momento temos os seguintes sabores de sorvete disponíveis: {self.flavors}'
         else:
             return "Estamos sem estoque atualmente!"
-            #print("Estamos sem estoque atualmente!")
 
     def find_flavor(self, flavor):
         """Verifica se o sabor informado está disponível."""
+        """
+        Bugs:
+        - O método está retornando a lista com todos os sabores. 
+        Neste caso deverá ser retornado somente o sabor pesquisado.
+        
+        Correção:
+        - Alterar {self.flavors} por flavor
+        
+        Remoção:
+        print(f"Temos no momento {self.flavors}!")
+        print(f"Não temos no momento {self.flavors}!")
+        print("Estamos sem estoque atualmente!"        
+        """
         if self.flavors:
             if flavor in self.flavors:
-                # bug: está retorenando a lista. Deveria ser somente flavor
                 return f"Temos no momento {flavor}!"
-                #print(f"Temos no momento {self.flavors}!")
             else:
               return f"Não temos no momento {flavor}!"
-                #print(f"Não temos no momento {self.flavors}!")
         else:
             return "Estamos sem estoque atualmente!"
-            #print("Estamos sem estoque atualmente!"
 
     def add_flavor(self, flavor):
         """Add o sabor informado ao estoque."""
-        # bug: não precisa verificar se a lista esta vazia
-        #if self.flavors:
+        """
+        Bugs:
+        - Não é necessário verificar se a lista está vazia para adicionar um sabor.
+        
+        Correção:
+        - Alterar print para return.
+        - Retirar a verificação se a lista de sabores está vazia.
+        
+        Remoção:
+        print(f"{flavor} adicionado ao estoque!")
+        print("\nSabor já disponivel!")
+        if self.flavors:
+        else:
+            print("Estamos sem estoque atualmente!")
+        """
         if flavor in self.flavors:
             return "Sabor já disponivel!"
-            # print("\nSabor já disponivel!")
         else:
             self.flavors.append(flavor)
             return f"{flavor} adicionado ao estoque!"
-            #print(f"{flavor} adicionado ao estoque!")
-        #else:
-        #    return "Estamos sem estoque atualmente!"
-            # print("Estamos sem estoque atualmente!")
