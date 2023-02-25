59d8feccb195c6d98179873773d834b235040102
tmm6 <tmm6@cesar.school>
2023-02-25 11:39:45 -0300

    Análise do código.

--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -9,11 +9,21 @@ class Restaurant:
 
     def describe_restaurant(self):
         """Imprima uma descrição simples da instância do restaurante."""
+        """
+        Correção: Ajuste de digitação nas mensagens de retorno. Trocar o print pelo return.
+        """
         print(f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}.")
         print(f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto.")
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
+        """
+        Bugs: 
+        - Se o restaurante está fechado, self.open deve receber True, que equivale a abrir o restaurante. Se deixar como
+        False, o restaurante sempre vai ficar fechado.
+        - Número de pessoas servidas está negativo. Não faz sentido ter -2 pessoas atenditas. Neste caso alterar para 0,
+         ou não utilizar, pois no contrutor já está definido como 0 pessoas.
+        """
         if not self.open:
             self.open = False
             self.number_served = -2
@@ -23,6 +33,11 @@ class Restaurant:
 
     def close_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está fechado para negócios."""
+        """
+        Bugs:
+        - Será necessário zerar o número de pessoas servidas ao fechar o restaurante, dado que ao abrir o número de
+        pessoas servidas também é fechada? Neste caso a opção é retirar self.number_served = 0.
+        """
         if self.open:
             self.open = False
             self.number_served = 0
@@ -32,6 +47,8 @@ class Restaurant:
 
     def set_number_served(self, total_customers):
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
+        """
+        """
         if self.open:
             self.number_served = total_customers
         else:
@@ -39,6 +56,13 @@ class Restaurant:
 
     def increment_number_served(self, more_customers):
         """Aumenta número total de clientes atendidos por este restaurante."""
+        """
+        Bug: 
+        - Não está sendo realizado o incremento da quantidade de pessoas. Neste caso pode ser perdido algumas 
+        informações. Por exemplo: Em um determinado momento o restaurante atendeu 10 pessoas, após um tempo X foram
+        atendidas 2 pessoas. Com self.number_served = more_customers, o self.number que antes seria == a 10 agora será 
+        == 2.         
+        """
         if self.open:
             self.number_served = more_customers
         else:
